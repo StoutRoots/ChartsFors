819ccdf1b19ba874c3a7bf9dbaa3dcd7da6c1b5d
StoutRoots <david.ramirez@tensordyn.com>
2015-01-12 22:37:01 -0800

    Update main.js
    update with Google chart data query

--- js/main.js
+++ js/main.js
@@ -6,12 +6,40 @@ function setZoom(s, e) {
 (function () {
     testmode = false;
 
-    function CSVToArray(strData, strDelimiter) {
-        strDelimiter = (strDelimiter || ",");
-        var objPattern = new RegExp(("(\\" + strDelimiter + "|\\r?\\n|\\r|^)(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
-        var arrData = [
-            []
-        ];
+//    function CSVToArray(strData, strDelimiter) {
+//        strDelimiter = (strDelimiter || ",");
+//        var objPattern = new RegExp(("(\\" + strDelimiter + "|\\r?\\n|\\r|^)(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+//        var arrData = [
+//            []
+//        ];
+
+// New function reads data from google charts
+
+//SEND REQUEST FOR DATA
+
+function initialize() {
+  var opts = {sendMethod: 'auto'};
+  // Replace the data source URL on next line with your data source URL.
+  var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1iBZowsCfXKO-g_Dc1xz8FJG_5m5q6ngPAmbb1XcdHXM/edit#gid=0', opts);
+
+  // Optional request to return only column C and the sum of column B, grouped by C members.
+  query.setQuery('select A, B');
+
+  // Send the query with a callback function.
+  query.send(handleQueryResponse);
+}
+
+//PROCESSING THE RESPONSE
+function handleQueryResponse(response) {
+
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  var data = response.getDataTable();
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, {width: 400, height: 240, is3D: true});
 
         var arrMatches = null;
         while (arrMatches = objPattern.exec(strData)) {
